c17e9dd28f75aa3c7d336e8aa2d404cef5792d23
panggeunho <72342550+banggeunho@users.noreply.github.com>
2020-11-24 22:03:11 +0900

    <ver2>jdbc 연동..

--- DB.java
+++ DB.java
@@ -1,12 +1,15 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.lang.*;
 
 public class DB {
-	void DB() {
+	void DB(int a_time) {
+		int result;
 		Connection con = null;
 		try
 		{
@@ -20,38 +23,33 @@ public class DB {
 		} catch(SQLException e) {
 			e.printStackTrace();
 		}
-
 		Statement stmt = null;
+		PreparedStatement pstmt = null;
 		ResultSet rs = null;
+		ResultSet prs = null;
 		try {
-			stmt = con.createStatement();
-			String sql = "select str_id, str_name, loc, menu_id, name, cost,cook_time, food_type  from store natural join menu";
-			rs = stmt.executeQuery(sql);
-	        ResultSetMetaData rsmd = rs.getMetaData();
-	        for (int i =1; i<=rsmd.getColumnCount(); i++) {
-	        	System.out.print(rsmd.getColumnName(i)+ "(");
-	        	System.out.print(rsmd.getColumnTypeName(i)+ ")\t");
-	        }
-	        System.out.println();
+			//stmt = con.createStatement();
+			String psql = "select str_name, loc, name, cost,cook_time from store natural join menu where cook_time < ?";
 			
+			pstmt= con.prepareStatement(psql);
+			a_time = a_time*60; //분단위를 초단위로 계산
+			pstmt.setInt(1, a_time); //query문에 사용.
+			rs = pstmt.executeQuery();
+			
+	        System.out.println();	        			
 			while(rs.next()) {
 				String str_name = rs.getString(1);
 				if(rs.wasNull()) str_name = "null";
 				String str_loc = rs.getString(2);
 				if(rs.wasNull()) str_loc = "null";
-				String menu_id = rs.getString(3);
-				if(rs.wasNull()) menu_id = "null";
-				String name = rs.getString(4);
+				String name = rs.getString(3);
 				if(rs.wasNull()) name = "null";
-				String cost  = rs.getString(5);
+				String cost  = rs.getString(4);
 				if(rs.wasNull()) cost = "null";
-				String cook_time = rs.getString(6);
+				String cook_time = rs.getString(5);
 				if(rs.wasNull()) cook_time = "null";
-				String food_type = rs.getString(7);
-				if(rs.wasNull()) food_type = "null";
-				System.out.println(str_name + "\t\t" + str_loc + "\t\t" + menu_id
-						  			+ "\t\t" + name + "\t\t" + cost + "\t\t" + cook_time
-						  			 + "\t\t" + food_type);
+				System.out.println("<"+str_name + ">\t\t\n" +"장소: "+ str_loc +
+											"\n 메뉴: "+ name + "\n비용: " + cost + "\n조리시간: " + cook_time);
 			}
 		} catch (SQLException e2) {
 			e2.printStackTrace();
--- main.java
+++ main.java
@@ -1,10 +1,22 @@
-
+import java.util.*;
+import java.io.*;
 public class main {
 
 	public static void main(String[] args) {
-		// this is main class.
-		System.out.println("this is main class.");
+		// Variable
+		Scanner scan = new Scanner(System.in);
+		int a_time = 0; //남는 시간
+		int t_time = 0; //총 걸리는 시간
+		String cur_loc = null; // 현재 위치
+		String des_loc = null; // 목적 위치
+		String store_loc = null; // 가게 위치
+		
+		a_time=scan.nextInt();
+		//
+		//floyd warshall 사용 후 db한테 시간 넘겨서 출력하는 것....
+		// 최소 경로 추가....
+		
 		DB jdbc = new DB();
-		jdbc.DB();
+		jdbc.DB(a_time);
 	}
 }
